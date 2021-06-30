1f8159001be10ee83a7d4c353fd403a78f36fa69
rigox100 <rigox100@gmail.com>
2021-06-29 21:30:47 -0500

    add logos

--- aviso_privacidad.php
+++ aviso_privacidad.php
@@ -245,7 +245,14 @@
 
                                 <li><i class="fa fa-envelope icolor"></i>&nbsp; reservaciones@sigatours.com</li>
                                 <li><i class="fa fa-address-book-o icolor"></i>&nbsp; Calle Sinaloa # 16 Colonia San Isidro II<br> C.P. 38670. Acámbaro, Gto.</li>
-
+                                <li>
+                                    <div class="logos mt-4">
+                                        <a href="#"><img src="img/gto.png" alt=""></a>
+                                        <a href="#"><img src="img/one.png" alt=""></a>
+                                        <a href="#"><img src="img/two.png" alt=""></a>
+                                        <a href="#"><img src="img/sectur.png" alt=""></a>
+                                    </div>
+                                </li>
 
                             </ul>
                         </div>
